a8a9b935815ed3cc9f057cf3634cb6a01d0771b0
ARAVIND RAM <aravindramvj@gmail.com>
2023-08-14 19:43:58 +0530

    UserDAO error fixed

--- src/test/java/com/fssa/freshtime/services/UserServiceTest.java
+++ src/test/java/com/fssa/freshtime/services/UserServiceTest.java
@@ -55,7 +55,7 @@ public class UserServiceTest {
     void testForgotPasswordValidInput() {
         UserService userService = new UserService();
 
-        assertDoesNotThrow(() -> userService.forgotPassword("aravind@freshword.com", "P@$$w0rd"));
+        assertDoesNotThrow(() -> userService.forgotPassword("aravind@freshworks.com", "P@$$w0rd"));
     }
 
     @Test
